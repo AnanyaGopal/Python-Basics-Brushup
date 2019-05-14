e8ddc81b11c86ed93540756c9c4216d82193d325
Ana <ananyagopal61@gmail.com>
2019-05-14 11:51:34 -0700

    Update string.py

--- string.py
+++ string.py
@@ -5,3 +5,8 @@
 # once you define a string, it is saved in memory char by char.
 
 s = 'hello daniel'
+
+# Python tries to save time in copying operations by defining the <string class>, <tuple class> as immutable.
+
+# Objects of built-in types like (int, float, bool, str, tuple, unicode) are immutable. 
+# Objects of built-in types like (list, set, dict) are mutable.
